dbc6ef217a737f941f76c9256270cc913dc43afc
David Rosevear <davidjrosevear@gmail.com>
2017-02-02 13:22:27 -0700

    Switching to SVG images

--- pressure.js
+++ pressure.js
@@ -1,11 +1,11 @@
 /* Image links and Sizes */
 
-var ballImage = "images/atom.png";
-var ballImageSize = 10;
-var ballContainerImage = "images/innertube.png";
+var ballImage = "ball.svg";
+var ballImageSize = 5;
+var ballContainerImage = "ball_container.svg";
 var ballContainerRimImage = "images/outertube.png";
-var ballContainerImageWidth = 500;
-var ballContainerImageHeight = 600;
+var ballContainerImageWidth = 379.2;
+var ballContainerImageHeight = 648.7;
 var lidImage = "images/disc.png";
 var lidImageWidth = 453;
 var lidImageHeight = 123;
@@ -17,9 +17,15 @@ var drawingElement = document.getElementById('pressureDrawing');
 drawingElement.style.width = ballContainerImageWidth + "px";
 drawingElement.style.height = ballContainerImageHeight + "px";
 
+
+var areaWidth = drawingElement.style.width.slice(0, -2);
+var areaHeight = drawingElement.style.height.slice(0, -2);
+
+var draw = SVG('pressureDrawing').svg(document.getElementById("svg_ball_container").outerHTML); //size(areaWidth, areaHeight);
+
 // Boundary limitations of the balls, relative to the container element
 var boundary = {
-    left: 25,
+    left: draw.select('g#back path').first().x(),
     top: 100,
     right: Number(drawingElement.style.width.slice(0, -2)) - 27,
     bottom: Number(drawingElement.style.height.slice(0, -2)) - 75
@@ -33,11 +39,6 @@ var boundary = {
 
 
 
-var areaWidth = drawingElement.style.width.slice(0, -2);
-var areaHeight = drawingElement.style.height.slice(0, -2);
-
-var draw = SVG('pressureDrawing').size(areaWidth, areaHeight);
-
 function Ball(startingLocation, speed, direction) {
     this.location = {
         x: startingLocation.x,
@@ -48,7 +49,8 @@ function Ball(startingLocation, speed, direction) {
         y: speed * Math.sin(direction)
     }
     
-    this.circle = draw.image(ballImage, ballImageSize, ballImageSize).cx(startingLocation.x).cy(startingLocation.y);
+    this.circle = draw.nested().svg(document.getElementById("svg_ball").outerHTML).size(ballImageSize, ballImageSize);
+    //this.circle = draw.image(ballImage, ballImageSize, ballImageSize).cx(startingLocation.x).cy(startingLocation.y);
     //this.circle = draw.circle(10).cx(startingLocation.x).cy(startingLocation.y).attr({fill: 'black'});
 }
 Ball.prototype.updateLocation = function() {
@@ -61,22 +63,28 @@ Ball.prototype.updateLocation = function() {
 }
 Ball.prototype.handleCollisions = function() {
     
+    var testboundary = document.getElementById('ballBoundary')
+    var boundaryLeft = testboundary.getAttribute('x');
+    var boundaryTop = testboundary.getAttribute('y');
+    var boundaryRight = boundaryLeft + testboundary.getAttribute('width');
+    var boundaryBottom = boundaryTop + testboundary.getAttribute('height');
+    
     // If the ball has crossed a boundary, reflect it back over the boundary, and change its direction
-    if (this.location.x < boundary.left) {
+    if (this.location.x < boundaryLeft) {
         this.velocity.x = -this.velocity.x;
-        this.location.x += 2 * (boundary.left - this.location.x);
+        this.location.x += 2 * (boundaryLeft - this.location.x);
     }
-    if (this.location.y < boundary.top) {
+    if (this.location.y < boundaryTop) {
         this.velocity.y = -this.velocity.y;
-        this.location.y += 2 * (boundary.top - this.location.y);
+        this.location.y += 2 * (boundaryTop - this.location.y);
     }
-    if (this.location.x > boundary.right) {
+    if (this.location.x > boundaryRight) {
         this.velocity.x = -this.velocity.x;
-        this.location.x += 2 * (boundary.right - this.location.x);
+        this.location.x += 2 * (boundaryRight - this.location.x);
     }
-    if (this.location.y > boundary.bottom) {
+    if (this.location.y > boundaryBottom) {
         this.velocity.y = -this.velocity.y;
-        this.location.y += 2 * (boundary.bottom - this.location.y);
+        this.location.y += 2 * (boundaryBottom - this.location.y);
     }
 }
 function generateBalls(n) {
@@ -104,13 +112,17 @@ function generateBalls(n) {
     return ballArray;
 }
 
-draw.image(ballContainerImage, ballContainerImageWidth, ballContainerImageHeight);
+//draw.image(ballContainerImage, ballContainerImageWidth, ballContainerImageHeight);
+//alert(document.getElementById("back"));
+//var containerBack = draw.nested().svg(document.getElementById("back").outerHTML);
 var balls = generateBalls(50);
 function moveBalls() {
     balls.forEach(function(ball) {
         ball.updateLocation();
     });
 }
-draw.image(lidImage, lidImageWidth, lidImageHeight).x(boundary.left - 2).y(boundary.top - (lidImageHeight/2));
-draw.image(ballContainerRimImage, ballContainerImageWidth, ballContainerImageHeight);
+//draw.image(lidImage, lidImageWidth, lidImageHeight).x(boundary.left - 2).y(boundary.top - (lidImageHeight/2));
+//var containerHandle = draw.nested().svg(document.getElementById("handle").outerHTML);
+//draw.image(ballContainerRimImage, ballContainerImageWidth, ballContainerImageHeight);
+//var containerFront = draw.nested().svg(document.getElementById("front").outerHTML);
 var pressureTimer = setInterval(moveBalls, 1000/30);
