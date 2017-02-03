3f288789bc011f344c2b2e1d2596a268386b38b4
David Rosevear <davidjrosevear@gmail.com>
2017-02-02 18:19:43 -0700

    Added comments and organized the code

--- pressure.js
+++ pressure.js
@@ -1,39 +1,62 @@
-/* Desired Sizes */
+/* START Desired Conditions - These can be modified */
 
+var numberOfBalls = 50;
 var ballImageSize = 5;
 var ballContainerImageWidth = 400; // The ratio is maintained below
 
-/* End of Desired Sizes */
+/* END of Desired Conditions */
 
-var ballContainerImageRatio = 648.7/379.2; // Viewbox Height/Width
+
+/* START Misc Setup */
+
+// Calculated sizes from the indicated width above
+var ballContainerImageRatio = 648.7/379.2; // ViewBox Height/Width
 var ballContainerImageHeight = ballContainerImageWidth * ballContainerImageRatio;
+
 
 // Container element for the svg tag
 var drawingElement = document.getElementById('pressureDrawing');
 drawingElement.style.width = ballContainerImageWidth + "px";
 drawingElement.style.height = ballContainerImageHeight + "px";
 
+
+// These seem to be redundant now, and should be changed
 var areaWidth = ballContainerImageWidth;
 var areaHeight = ballContainerImageHeight;
+
 
 // Store the outmost svg in SVG.js form
 var draw = SVG('pressureDrawing').svg(document.getElementById("svg_ball_container").outerHTML); //size(areaWidth, areaHeight);
 
-// Handling of handle movements using SVG.js
+/* END Misc Setup */
+
+
+/* START Handling of handle movements using SVG.js */
+
 var handle = draw.select('#handle').first();
 /*handle.draggable(function(x, y) {
     return {y: y >= 0 && y < boundary.bottom - 20}
 });*/
-var handleHeld = false;
+var handleHeld = false; // Flag for mouse events
 handle.mousedown(function(e) {
     handleHeld = true;
 })
 document.body.onmousemove = function(e) {
+    
+    // If the user is currently 'holding the handle'
     if (handleHeld) {
+        
+        // Calculate the movement within the viewBox
         var distance = e.movementY * 648.7 / ballContainerImageHeight;
+        
+        // If we're trynig to move the handle within the boundary
         var handlePosition = handle.transform('y');
         if ((distance < 0 && handlePosition > 0) || (distance > 0 && handlePosition < boundary.bottom - 25)) {
+            
+            // Move the handle
             handle.transform({ y: distance, relative: true});
+            
+            // Align the top of the ball boundary
             boundary.top += distance;
         }
     }
@@ -42,12 +65,17 @@ document.body.onmouseup = function(e) {
     handleHeld = false;
 }
 
+/* END Handling of handle movements using SVG.js */
 
+
+/* START boundary object */
 
 // Boundary limitations of the balls, relative to the container element
 var boundary = {
     top: 0
 }
+
+// Updates the bounds
 function updateBoundary() {
     var testboundary = document.getElementById('ballBoundary');
     var handledisc = document.getElementById('handle');//.getElementsByTagName('ellipse').item(0);
@@ -56,27 +84,46 @@ function updateBoundary() {
     boundary.bottom = Number(testboundary.getAttribute('height'));
 }
 
+/* END boundary object */
 
+
+/* START Ball Prototype */
+
+/* Constructor Properties */
 function Ball(startingLocation, speed, direction) {
+    
+    // Holds the center coordinates
     this.location = {
         x: startingLocation.x,
         y: startingLocation.y
     };
+    
+    // Velocity vector
     this.velocity = {
         x: speed * Math.cos(direction),
         y: speed * Math.sin(direction)
     }
     
+    // Create the ball copy in svg using SVG.js
     this.circle = draw.select('#svg_ball_boundary').first().nested().svg(document.getElementById("svg_ball").outerHTML).size(ballImageSize, ballImageSize);
+    
 }
+/* Constructor Methods */
+// Use the ball's velocity and collision handling to update its location
 Ball.prototype.updateLocation = function() {
+    
+    // Update the ball's location info
     this.location.x += this.velocity.x;
     this.location.y += this.velocity.y;
-    //this.circle.dmove(this.velocity.x, this.velocity.y);
     
+    // Check for and handle collisions
     this.handleCollisions();
+    
+    // Move the ball to new location
     this.circle.cx(this.location.x).cy(this.location.y);
+    
 }
+// Check for collisions and modify location and velocity vector accordingly
 Ball.prototype.handleCollisions = function() {
     
     // If the ball has crossed a boundary, reflect it back over the boundary, and change its direction
@@ -97,12 +144,18 @@ Ball.prototype.handleCollisions = function() {
         this.location.y += 2 * (boundary.bottom - this.location.y);
     }
 }
+
+/* END Ball Prototype */
+
+
+/* START Other ball code */
+
+// Will generate n balls according to requirements
 function generateBalls(n) {
     
     // Create a speed variable holding random speed
     var speedMultiplier = 5;
     var initialSpeed = Math.random() * speedMultiplier;
-    
     
     // Use a loop to creae n Balls, with random location and direction, but same speed
     var ballArray = [];
@@ -119,17 +172,27 @@ function generateBalls(n) {
         ballArray.push(newBall);
     }
     
+    // Return the created set of n balls
     return ballArray;
 }
 
-
-var balls = generateBalls(50);
-function moveBalls() {
-    console.log(document.getElementById('handle').getElementsByTagName('ellipse').item(0).getAttribute('cy'));
+// Update the boundary and move the balls one frame
+function moveBalls(balls) {
     updateBoundary();
     balls.forEach(function(ball) {
         ball.updateLocation();
     });
 }
 
-var pressureTimer = setInterval(moveBalls, 1000/30);
+/* END Other ball code */
+
+
+/* START Getting the simulation going */
+
+// Store a generated set of balls
+var balls = generateBalls(numberOfBalls);
+
+// Start the animation
+var pressureTimer = setInterval(moveBalls(balls), 1000/30);
+
+/* END Getting the simulation going */
